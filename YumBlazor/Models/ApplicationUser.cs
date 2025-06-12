b6bdca0f0950a28f52afd2436b1c7cfc2d0b7faa
Bryan Dellinger <bryandellingeredu@gmail.com>
2025-06-12 14:29:44 -0400

    fixing sonarcube

--- YumBlazor/Models/ApplicationUser.cs
+++ YumBlazor/Models/ApplicationUser.cs
@@ -1,8 +1,8 @@
 using Microsoft.AspNetCore.Identity;
-
 namespace YumBlazor.Models
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S2094:Remove this empty class...", Justification = "Required for Identity customization")]
     public class ApplicationUser : IdentityUser
     {
     }
